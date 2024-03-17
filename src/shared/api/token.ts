8b583e3a266fcabb3a4deda3fb4df282f1d7b0c0
Dmytry64 <nosdm64@gmail.com>
2024-03-17 13:49:40 +0300

    Добавляет ДЗ Контроль и ограничения ч2

---
+++ src/shared/api/token.ts
@@ -0,0 +1,17 @@
+const TOKEN_KEY_NAME = 'six-cities-token';
+
+export type Token = string;
+
+export const getToken = () : Token => {
+  const token = localStorage.getItem(TOKEN_KEY_NAME);
+  return token ?? '';
+};
+
+export const seveToken = (token : Token) => {
+  localStorage.setItem(TOKEN_KEY_NAME, token);
+};
+
+export const dropToken = () : void => {
+  localStorage.removeItem(TOKEN_KEY_NAME);
+};
+
