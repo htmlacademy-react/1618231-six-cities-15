d2a101afd440e6e795d15d64300aae97f5bcf5d6
Dmytry64 <nosdm64@gmail.com>
2024-03-24 07:46:35 +0300

    ДЗ 7.15

--- src/app/api-actions.ts
+++ src/app/api-actions.ts
@@ -1,17 +1,18 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { AxiosInstance } from 'axios';
 import { createAPI } from 'src/shared/api/apiRequest';
-import { DetailedOfferType, OfferType, UserReviewsType } from 'src/shared/app-types';
+import { AuthData, DetailedOfferType, OfferType, UserReviewsType, UserType } from 'src/shared/app-types';
 import { ApiActions, FetchRoutes } from 'src/shared/constans';
 
 
 const api = createAPI();
 
 export const fetchOffersList = createAsyncThunk(ApiActions.DataFetchOffers, async () => {
-  const {data} = await api.get<OfferType[]>(FetchRoutes.Offers);
+  const { data } = await api.get<OfferType[]>(FetchRoutes.Offers);
   return data;
 });
 
-export const fetchReviewsList = createAsyncThunk(ApiActions.DataFetchReviews, async (offerId : string) => {
+export const fetchReviewsList = createAsyncThunk(ApiActions.DataFetchReviews, async (offerId: string) => {
   const { data } = await api.get<UserReviewsType[]>(`${FetchRoutes.Comments}/${offerId}`);
   return data;
 });
@@ -26,3 +27,17 @@ export const fetchNearbyOffers = createAsyncThunk(ApiActions.DataFetchNearbyOffe
   return data;
 });
 
+export const fetchAuthStatus = createAsyncThunk(ApiActions.AuthFetchStatus, async () => {
+  try {
+    const { data } = await api.get<UserType>(FetchRoutes.Login);
+    return data;
+  } catch {
+    const {error} = Response;
+    console.log(error());
+  }
+});
+
+// export const fetchUserLogin = createAsyncThunk<void, AuthData>(ApiActions.UserFetchLogin, async (loginData) => {
+//   const {data} = await api.post<UserType>(FetchRoutes.Login, loginData);
+//   return data;
+// })
--- src/pages/layout/ui/layout.tsx
+++ src/pages/layout/ui/layout.tsx
@@ -1,38 +1,60 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
+import { AppRoutes, AutorizationStatus } from 'src/shared/constans';
+import { useAppSelector } from 'src/shared/hooks';
+import cn from 'classnames';
 
-const Layout = (): JSX.Element => (
-  <div className="page page--gray page--main">
-    <header className="header">
-      <div className="container">
-        <div className="header__wrapper">
-          <div className="header__left">
-            <a className="header__logo-link header__logo-link--active">
-              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-            </a>
+const Layout = (): JSX.Element => {
+  const {pathname} = useLocation();
+  const statusAuth = useAppSelector((state) => state.userState.status);
+
+  return (
+    <div className={cn('page', { 'page--gray page--main': pathname === AppRoutes.Main as string}, { 'page--gray page--login': pathname === AppRoutes.Login as string})}>
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link header__logo-link--active">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+              </a>
+            </div>
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                {statusAuth !== AutorizationStatus.Auth &&
+                  <li className="header__nav-item user">
+                    <Link
+                      className="header__nav-link header__nav-link--profile"
+                      to={AppRoutes.Login}
+                    >
+                      <div className="header__avatar-wrapper user__avatar-wrapper">
+                      </div>
+                      <span className="header__login">Sign in</span>
+                    </Link>
+                  </li>}
+
+                {statusAuth === AutorizationStatus.Auth &&
+                  <>
+                    <li className="header__nav-item user">
+                      <a className="header__nav-link header__nav-link--profile" href="#">
+                        <div className="header__avatar-wrapper user__avatar-wrapper">
+                        </div>
+                        <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                        <span className="header__favorite-count">3</span>
+                      </a>
+                    </li>
+                    <li className="header__nav-item">
+                      <a className="header__nav-link" href="#">
+                        <span className="header__signout">Sign out</span>
+                      </a>
+                    </li>
+                  </>}
+              </ul>
+            </nav>
           </div>
-          <nav className="header__nav">
-            <ul className="header__nav-list">
-              <li className="header__nav-item user">
-                <a className="header__nav-link header__nav-link--profile" href="#">
-                  <div className="header__avatar-wrapper user__avatar-wrapper">
-                  </div>
-                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  <span className="header__favorite-count">3</span>
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a className="header__nav-link" href="#">
-                  <span className="header__signout">Sign out</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
         </div>
-      </div>
-    </header>
-    <Outlet />
-  </div>
-);
-
+      </header>
+      <Outlet />
+    </div>
+  );
+};
 
 export default Layout;
